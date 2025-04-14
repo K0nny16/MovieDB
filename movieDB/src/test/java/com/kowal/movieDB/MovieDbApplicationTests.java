07843cd52a331b9e1157b2746c9d57cbf0de8263
Karl Kowal <kowal.karl99@gmail.com>
2025-04-14 15:09:07 +0200

    added: ci

--- movieDB/src/test/java/com/kowal/movieDB/MovieDbApplicationTests.java
+++ movieDB/src/test/java/com/kowal/movieDB/MovieDbApplicationTests.java
@@ -1,13 +1,34 @@
 package com.kowal.movieDB;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MovieDbApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void healthEndpointShouldReturnOk() {
+		ResponseEntity<String> response = restTemplate.getForEntity(
+				"http://localhost:" + port + "/actuator/health", 
+				String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
